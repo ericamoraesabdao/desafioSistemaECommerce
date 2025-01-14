19e60c04a45b0522da63a799a4e22ded6f2572ed
Erica <erica.abdao@zup.com.br>
2025-01-14 20:37:51 -0300

    Primeiro commite Hello World com Spring Boot

---
+++ helloWord/src/main/java/com/example/helloWord/controllers/HelloWordController.java
@@ -0,0 +1,12 @@
+package com.example.helloWord.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordController {
+    @GetMapping ("/")
+    public String healthCheck(){
+        return "Hello World!";
+    }
+}
